c39c1b9145fbfa4e320c99482c346370d3b87430
bhuvaneshvar <bhuvaneshvar6@outlook.com>
2020-10-11 00:54:57 +0530

    added

--- src/main/kotlin/com/bhuvancom/ecom/ServletInitializer.kt
+++ src/main/kotlin/com/bhuvancom/ecom/ServletInitializer.kt
@@ -2,11 +2,13 @@ package com.bhuvancom.ecom
 
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.stereotype.Controller
 
+@Controller
 class ServletInitializer : SpringBootServletInitializer() {
 
-	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-		return application.sources(EComApplication::class.java)
-	}
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(EComApplication::class.java)
+    }
 
 }
