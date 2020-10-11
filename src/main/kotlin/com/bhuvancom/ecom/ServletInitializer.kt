14550638d5818cb668d4405a3c449c961af52a2b
app-kode <72460271+app-kode@users.noreply.github.com>
2020-10-11 12:10:20 +0530

    Update ServletInitializer.kt

--- src/main/kotlin/com/bhuvancom/ecom/ServletInitializer.kt
+++ src/main/kotlin/com/bhuvancom/ecom/ServletInitializer.kt
@@ -4,12 +4,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.stereotype.Component
 
-@Component
-class ServletInitializer : SpringBootServletInitializer() {
+//@Component
+class ServletInitializer
+// : SpringBootServletInitializer() 
+{
 
-    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-        return application.sources(EComApplication::class.java)
-    }
-
+   // override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+     //   return application.sources(EComApplication::class.java)
+    //}
 
 }
